07e9631d28b176fa29457de4ae3b24ee159af396
Balázs Máté <balazsm86@gmail.com>
2016-10-17 14:45:10 +0200

    Modified some functions

--- 100Doors.py
+++ 100Doors.py
@@ -1,36 +1,31 @@
 # ========== Functions ==========
 
+# Create an list with the close doors (0: closed, 1: opened)
 def Initialization():
     Doors = []
     for i in range(100):
-        Doors.append(0)
+        Doors.append(0)    # All doors are closed
     return Doors
 
-
+# If the door is open/close, the function close/open it
 def ToogleDoor(doors):
-    step = 1
-    while step <=100:
-        for i in range(0, len(doors), step):
-            if doors[i] == 0:
+    for step in range(1, len(doors)):    # This loop changes the step
+        for i in range(step, len(doors), step):
+            if doors[i] == 0:   # If the door is close, opens it
                 doors[i] = 1
             else:
-                doors[i] = 0
-        step += 1
+                doors[i] = 0    # If the door is open, close it
     return doors
-    #print(doors)
 
+# Count the open doors, and stores its index in a list
 def CountOpenDoors(doors):
-    OpenDoors = []
+    OpenDoorsIndex = []
     for i in range(len(doors)):
         if doors[i] == 1:
-            OpenDoors.append(i)
-    print("The following doors are open: ")
-    for i in range(len(OpenDoors)):
-        print(OpenDoors[i])
-
-
+            OpenDoorsIndex.append(str(i))
+    print("The following doors are open: " + ", ".join(OpenDoorsIndex))    # It prints the list elements without brackets
 
 # ========== Main ==========
 
-#Initialization()
+# Calling the functions
 CountOpenDoors(ToogleDoor(Initialization()))
