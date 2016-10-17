26c20f31119cc4225de8b53e30ebb42070a65a26
Balázs Máté <balazsm86@gmail.com>
2016-10-17 11:21:52 +0200

    Added Initialization() function

---
+++ 100Doors.py
@@ -0,0 +1,15 @@
+# ========== Functions ==========
+
+def Initialization:
+    Doors = []
+    for i in range(100):
+        Doors.append(0)
+    return Doors
+
+
+def ToogleDoor(doors):
+
+
+
+
+# ========== Main ==========
